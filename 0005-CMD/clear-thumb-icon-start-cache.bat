66979e59052d9c97282803835b98593215fc6db8
FadeMind <fademind@gmail.com>
2020-06-24 19:47:42 +0200

    20200624

--- 0005-CMD/clear-thumb-icon-start-cache.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-taskkill /IM explorer.exe /F
-cd /d %userprofile%\AppData\Local\Microsoft\Windows\Explorer
-attrib -H *.db
-del /F *.db
-dir
-pause
-start explorer
-dir
---
+++ 0005-CMD/reset-icon-cache-fix.cmd
@@ -0,0 +1,39 @@
+@echo OFF
+:: Check if we are administrator. If not, exit immediately.
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+	echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+	
+:MJOB
+ie4uinit.exe -show
+taskkill /F /IM explorer.exe
+cd /d "%userprofile%\AppData\Local\Microsoft\Windows\Explorer"
+attrib -h iconcache_*.db
+DEL /A /Q "%localappdata%\IconCache.db"
+DEL /A /F /Q "%localappdata%\Microsoft\Windows\Explorer\iconcache*"
+start explorer
+pause
